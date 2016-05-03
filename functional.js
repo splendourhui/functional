221d35c5c41827ca9e5f21039ccff52f36603cdc
Splendour <splendourhui@foxmail.com>
2016-05-03 20:13:29 +0800

    [feature] reduce, throttle 和 debounce 函数

---
+++ functional.js
@@ -0,0 +1,58 @@
+/**
+* @Author: SplendourHui
+* @Date:   2016-05-03 14:41
+* @Last modified by:   SplendourHui
+* @Last modified time: 2016-05-03 20:08
+*/
+
+
+
+'use strict';
+
+/**
+* 将函数转化为多参数操作的函数
+* @async 是否异步函数
+*/
+exports.reduce = (fn, async) => {
+  if (async) {
+    return function(...args) {
+      return args.reduce((a, b) => {
+        return Promise.resolve(a).then((v) => fn.call(this, v, b));
+      });
+    }
+  } else {
+    return function(...args) {
+      return args.reduce(fn.bind(this));
+    }
+  }
+}
+
+/**
+* 限制函数调用的频率
+* @wait 限制两次调用之间的间隔
+*/
+exports.throttle = (fn, wait) => {
+  let timer = null;
+  return function(...args) {
+    if (!timer) {
+      timer = setTimeout(() => {
+        clearTimeout(timer);
+      }, wait);
+      return fn.apply(this, args);
+    }
+  }
+}
+
+/**
+* 使函数延时一定时间后触发，重复调用会重置计时
+* @delay 延时时间
+*/
+exports.debounce = (fn, delay) => {
+  let timer = null;
+  return function(...args) {
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+      fn.apply(this, args);
+    }, delay);
+  }
+}
---
+++ test.js
@@ -0,0 +1,33 @@
+/**
+* @Author: SplendourHui
+* @Date:   2016-05-03 14:42
+* @Last modified by:   SplendourHui
+* @Last modified time: 2016-05-03 20:07
+*/
+
+
+
+'use strict';
+const functional = require('./functional');
+
+function add(x, y) {
+  return x + y;
+}
+
+function asyncAdd(x, y) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => resolve(x + y), 100);
+  });
+}
+
+add = functional.reduce(add);
+asyncAdd = functional.reduce(asyncAdd, true);
+console.log(add(1, 2, 3, 4));
+asyncAdd(1, 2, 3, 4, 5).then(result => console.log(result));
+
+const debounce = functional.debounce(() => {
+  console.log('debounce test');
+}, 1000);
+
+debounce();
+debounce();
